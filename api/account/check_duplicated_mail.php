18c1fb472afa05eac2b5b07c5e3ad6f128c2d1d5
sliu9412 <sliu9412@conestogac.on.ca>
2023-06-08 19:58:52 -0400

    fix check duplicated mail bug

--- api/account/check_duplicated_mail.php
+++ api/account/check_duplicated_mail.php
@@ -4,16 +4,20 @@ require("../../utils/connect_db.php");
 // db table name
 function CheckDuplicateMail()
 {
-    $table_name = "userinfo";
-    $email = $_POST["email"];
-    $query = $GLOBALS["db"]->prepare("SELECT COUNT(email) as count FROM $table_name WHERE email = :email");
-    $query->bindValue(":email", $email);
-    $query->execute();
-    $count = $query->fetch()["count"];
-    if (intval($count) == 0) {
-        JsonResponse(true);
+    if (isset($_POST["email"])) {
+        $table_name = "userinfo";
+        $email = $_POST["email"];
+        $query = $GLOBALS["db"]->prepare("SELECT COUNT(email) as count FROM $table_name WHERE email = :email");
+        $query->bindValue(":email", $email);
+        $query->execute();
+        $count = $query->fetch()["count"];
+        if (intval($count) == 0) {
+            JsonResponse(true);
+        } else {
+            JsonResponse(false);
+        }
     } else {
-        JsonResponse(false);
+        throw new Exception("Invalid request", 400);
     }
 }
 
