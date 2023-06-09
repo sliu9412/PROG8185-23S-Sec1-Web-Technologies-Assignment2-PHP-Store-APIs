1c2154f1e5dbc6b8bdef70824c96a74e10dbfb3a
sliu9412 <sliu9412@conestogac.on.ca>
2023-06-09 14:41:45 -0400

    Mock data for userinfo and product

---
+++ utils/mock_data.php
@@ -0,0 +1,88 @@
+<?php
+$mock_users = [
+    [
+        "username" => "user1",
+        "password" => password_hash("1234", PASSWORD_DEFAULT),
+        "province" => "Ontario",
+        "city" => "Waterloo",
+        "zipcode" => "N2J 2W2",
+        "email" => "example1@mail.com",
+    ],
+    [
+        "username" => "user2",
+        "password" => password_hash("1234", PASSWORD_DEFAULT),
+        "province" => "Ontario",
+        "city" => "Waterloo",
+        "zipcode" => "N2J 2W2",
+        "email" => "example2@mail.com",
+    ],
+    [
+        "username" => "user3",
+        "password" => password_hash("1234", PASSWORD_DEFAULT),
+        "province" => "Ontario",
+        "city" => "Waterloo",
+        "zipcode" => "N2J 2W2",
+        "email" => "example3@mail.com",
+    ],
+    [
+        "username" => "user4",
+        "password" => password_hash("1234", PASSWORD_DEFAULT),
+        "province" => "Ontario",
+        "city" => "Waterloo",
+        "zipcode" => "N2J 2W2",
+        "email" => "example4@mail.com",
+    ],
+    [
+        "username" => "user5",
+        "password" => password_hash("1234", PASSWORD_DEFAULT),
+        "province" => "Ontario",
+        "city" => "Waterloo",
+        "zipcode" => "N2J 2W2",
+        "email" => "example5@mail.com",
+    ]
+];
+
+$mock_products = [
+    [
+        "name" => "product 1",
+        "price" => 1,
+        "img" => "images/product 1",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+    [
+        "name" => "product 2",
+        "price" => 2,
+        "img" => "images/product 2",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+    [
+        "name" => "product 3",
+        "price" => 3,
+        "img" => "images/product 3",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+    [
+        "name" => "product 4",
+        "price" => 4,
+        "img" => "images/product 4",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+    [
+        "name" => "product 5",
+        "price" => 5,
+        "img" => "images/product 5",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+    [
+        "name" => "product 6",
+        "price" => 6,
+        "img" => "images/product 6",
+        "shipping_cost" => 2.0,
+        "description" => "This is a description of this product"
+    ],
+];
