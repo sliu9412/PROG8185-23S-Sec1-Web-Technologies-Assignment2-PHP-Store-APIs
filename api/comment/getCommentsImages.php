facb2afb673ed45fe07f9d68553b33e9999ce443
Eugenio Contreras <eugenio@agilesoft.cl>
2023-06-13 14:31:37 -0400

    fix product images

--- api/comment/getCommentsImages.php
+++ api/comment/getCommentsImages.php
@@ -1,11 +1,11 @@
 <?php
-require_once("../../provider/comment/comment_provider.php");
+require_once("../../provider/comment/commentImage_provider.php");
 
 function CommentImagesDisplay(...$args)
 {
     header("Content-Type: JSON");
     
-
+    $comment_id = htmlspecialchars($_GET["comment_id"]);
     $response = getCommentImages($comment_id);
     if($response){
         echo json_encode($response, JSON_PRETTY_PRINT);
